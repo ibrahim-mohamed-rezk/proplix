163e46b30cf433f727b332753baf66443177bf8d
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-05-18 10:13:28 +0300

    Refactor imports in HomeOne component for consistency and add data fetching functionality for home data.

--- src/components/dashboard/index/DashboardChart.tsx
+++ src/components/dashboard/index/DashboardChart.tsx
@@ -38,5 +38,3 @@ const BarChart = () => {
 };
 
 export default BarChart;
-
-
--- src/components/homes/home-one/index.tsx
+++ src/components/homes/home-one/index.tsx
@@ -1,22 +1,32 @@
-import FooterOne from "@/layouts/footers/FooterOne"
-import HeaderOne from "@/layouts/headers/HeaderOne"
-import Banner from "./Banner"
-import Feedback from "./Feedback"
-import BLockFeatureOne from "./BLockFeatureOne"
-import BLockFeatureTwo from "./BLockFeatureTwo"
-import BLockFeatureThree from "./BLockFeatureThree"
-import Property from "./Property"
-import FancyBannerOne from "./FancyBannerOne"
-import AgentArea from "./AgentArea"
-import BLockFeatureFour from "./BLockFeatureFour"
-import BLockFeatureFive from "./BLockFeatureFive"
-import FancyBannerThree from "./FancyBannerThree"
-import FancyBanner from "@/components/common/FancyBanner"
-import { cookies } from "next/headers"
-
+import FooterOne from "@/layouts/footers/FooterOne";
+import HeaderOne from "@/layouts/headers/HeaderOne";
+import Banner from "./Banner";
+import Feedback from "./Feedback";
+import BLockFeatureOne from "./BLockFeatureOne";
+import BLockFeatureTwo from "./BLockFeatureTwo";
+import BLockFeatureThree from "./BLockFeatureThree";
+import Property from "./Property";
+import FancyBannerOne from "./FancyBannerOne";
+import AgentArea from "./AgentArea";
+import BLockFeatureFour from "./BLockFeatureFour";
+import BLockFeatureFive from "./BLockFeatureFive";
+import FancyBannerThree from "./FancyBannerThree";
+import FancyBanner from "@/components/common/FancyBanner";
+import { cookies } from "next/headers";
+import { getData } from "@/libs/server/backendServer";
 
 const HomeOne = async () => {
   const token = (await cookies()).get("token")?.value;
+  const feachData = async () => {
+    try {
+      const response = await getData("home", {}, {});
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  const homeData = await feachData();
+  
   return (
     <>
       <HeaderOne token={token} style={false} />
@@ -37,4 +47,4 @@ const HomeOne = async () => {
   );
 };
 
-export default HomeOne
+export default HomeOne;
