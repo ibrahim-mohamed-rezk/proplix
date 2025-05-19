236fd865032bd3edc6115d9c9e7c238e1874914b
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-05-19 06:02:11 +0300

    Remove ServiceTwo component and its associated features, including BLockFeatureOne, BLockFeatureTwo, and BLockFeatureThree, to streamline the codebase and eliminate unused components. This cleanup improves maintainability and reduces complexity in the project.

--- src/components/inner-pages/services/service-one/index.tsx
+++ src/components/inner-pages/services/service-one/index.tsx
@@ -6,7 +6,7 @@ import HomeBLockFeatureOne from "@/components/homes/home-one/BLockFeatureOne"
 import BlockFeatureTwo from "./BlockFeatureTwo"
 import Feedback from "@/components/homes/home-one/Feedback"
 import FancyBanner from "./FancyBanner"
-import BLockFeatureTwo from "@/components/homes/home-four/BLockFeatureTwo"
+// import BLockFeatureTwo from "@/components/homes/home-four/BLockFeatureTwo"
 import FancyBannerCommon from "@/components/common/FancyBanner"
 
 const ServiceOne = () => {
@@ -19,7 +19,7 @@ const ServiceOne = () => {
          <BlockFeatureTwo style={false} />
          <FancyBanner />
          <Feedback/>
-         <BLockFeatureTwo style={true} />
+         {/* <BLockFeatureTwo style={true} /> */}
          <FancyBannerCommon />
          <FooterFour />
       </>
