a65293c817ceed254ba8edf3898e362b8519ad74
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2025-09-24 09:43:35 +0300

    feat(about-us): implement internationalization for BLockFeatureOne component
    - Refactored BLockFeatureOne to utilize translations for dynamic content rendering.
    - Updated the component to fetch translations for titles, descriptions, and button text.
    - Enhanced Arabic and English locale files with new translations for the About Proplix section, including titles and descriptions.

--- src/components/inner-pages/about-us/about-us-one/BLockFeatureOne.tsx
+++ src/components/inner-pages/about-us/about-us-one/BLockFeatureOne.tsx
@@ -1,75 +1,67 @@
-import Image from "next/image"
-import Link from "next/link"
-
-import titleShape from "@/assets/images/shape/title_shape_06.svg";
 import Count from "@/components/common/Count";
+import { Link } from "@/i18n/routing";
+import { getTranslations } from "next-intl/server";
 
-interface ContentType {
-   sub_title: string;
-   desc_1: React.ReactElement;
-   title_1: string;
-   title_2: string;
-   desc_2: React.ReactElement;
-   desc_3: React.ReactElement;
-}
-
-const feature_content: ContentType = {
-   sub_title: "About us",
-   desc_1: (<>Our founders Dustin Moskovitz and Justin Rosenstein met while leading Engineering .</>),
-   title_1: "Who we are?",
-   title_2: "Our Mission",
-   desc_2: (<>Our founders Dustin Moskovitz & Justin Rosenstein met  leading Engineering teams at Facebook. As operations scaled, they grew frustrated by how difficult coordinate</>),
-   desc_3: (<>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod incididunt ut labore et dolore aliqua.</>),
-}
-
-const { sub_title, desc_1, title_1, title_2, desc_2, desc_3 } = feature_content;
-
-const BLockFeatureOne = () => {
-   return (
-      <div className="block-feature-two mt-150 xl-mt-100">
-         <div className="container">
-            <div className="row gx-xl-5">
-               <div className="col-lg-6 wow fadeInLeft">
-                  <div className="me-xxl-4">
-                     <div className="title-one mb-60 lg-mb-40">
-                        <div className="upper-title">{sub_title}</div>
-                        <h3>Secure your <span>family&apos;s<Image src={titleShape} alt="" className="lazy-img" /></span> Dream home.</h3>
-                        <p className="fs-22">{desc_1}</p>
-                     </div>
-                     <Link href="/contact" className="btn-two">Contact Us</Link>
-                     <div className="counter-wrapper border-top pt-40 md-pt-10 mt-65 md-mt-40">
-                        <div className="row">
-                           <div className="col-xxl-6 col-sm-5">
-                              <div className="counter-block-one mt-20">
-                                 <div className="main-count fw-500 color-dark"><span className="counter"><Count number={1.2} /></span>%</div>
-                                 <span>Low interest rate</span>
-                              </div>
-                           </div>
-                           <div className="col-xxl-6 col-sm-7">
-                              <div className="counter-block-one mt-20">
-                                 <div className="main-count fw-500 color-dark">$<span className="counter"><Count number={1.3} /></span>b+</div>
-                                 <span>Cumulative trading volume</span>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+const BLockFeatureOne = async () => {
+  const t = await getTranslations("endUser.about");
+  return (
+    <div className="block-feature-two mt-150 xl-mt-100">
+      <div className="container">
+        <div className="row gx-xl-5">
+          <div className="col-lg-6 wow fadeInLeft">
+            <div className="me-xxl-4">
+              <div className="title-one mb-60 lg-mb-40">
+                <div className="upper-title">{t("title")}</div>
+                <h3>{t("sub_title")}</h3>
+                <p className="fs-22">{t("description")}</p>
+              </div>
+              <Link href="/contact" className="btn-two">
+              {t("contact_us")}
+              </Link>
+              <div className="counter-wrapper border-top pt-40 md-pt-10 mt-65 md-mt-40">
+                <div className="row">
+                  <div className="col-xxl-6 col-sm-5">
+                    <div className="counter-block-one mt-20">
+                      <div className="main-count fw-500 color-dark">
+                        <span className="counter">
+                          <Count number={8} />
+                        </span>
+                        %
+                      </div>
+                      <span>{t("Low interest rate")}</span>
+                    </div>
                   </div>
-               </div>
-
-               <div className="col-lg-6 wow fadeInRight">
-                  <div className="block-two md-mt-40">
-                     <div className="bg-wrapper">
-                        <h5>{title_1}</h5>
-                        <p className="fs-22 lh-lg mt-20">{desc_2}</p>
-                        <h5 className="top-line">{title_2} </h5>
-                        <p className="fs-22 lh-lg mt-20">{desc_3}</p>
-                     </div>
+                  <div className="col-xxl-6 col-sm-7">
+                    <div className="counter-block-one mt-20">
+                      <div className="main-count fw-500 color-dark">
+                        $
+                        <span className="counter">
+                          <Count number={1.3} />
+                        </span>
+                        b+
+                      </div>
+                      <span>{t("cumulative_trading_volume")}</span>
+                    </div>
                   </div>
-               </div>
+                </div>
+              </div>
             </div>
-         </div>
-      </div>
-   )
-}
+          </div>
 
-export default BLockFeatureOne
+          <div className="col-lg-6 wow fadeInRight">
+            <div className="block-two md-mt-40">
+              <div className="bg-wrapper">
+                <h5>{t("title_1")}</h5>
+                <p className="fs-22 lh-lg mt-20">{t("desc_2")}</p>
+                <h5 className="top-line">{t("title_2")} </h5>
+                <p className="fs-22 lh-lg mt-20">{t("desc_3")}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BLockFeatureOne;
